63f1dfae07185d014634ac8897865007328f45e9
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-19 02:34:52 -0500

    routes now protected

--- playground/src/app/app-routing.module.ts
+++ playground/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { LoginComponent } from './login/login.component';
 import { LandingComponent } from './landing/landing.component';
 import { ErrorComponent } from './error/error.component';
 import { ListTodosComponent } from './list-todos/list-todos.component';
-import { LogoutComponent } from "./logout/logout.component"
+import { LogoutComponent } from './logout/logout.component'
+import { RouteGuardService } from './service/route-guard.service';
 
 // welcome
 const routes: Routes = [
@@ -12,10 +13,10 @@ const routes: Routes = [
 
   { path: 'login', component: LoginComponent },
   {
-    path: 'landing/:name', component: LandingComponent
+    path: 'landing/:name', component: LandingComponent, canActivate: [RouteGuardService]
   },
-  { path: 'todos', component: ListTodosComponent },
-  { path: 'logout', component: LogoutComponent},
+  { path: 'todos', component: ListTodosComponent, canActivate: [RouteGuardService] },
+  { path: 'logout', component: LogoutComponent, canActivate: [RouteGuardService] },
   {
     path: '**', component: ErrorComponent
   }
