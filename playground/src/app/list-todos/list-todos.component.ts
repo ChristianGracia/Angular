30c4834098082011fe9babd673b2f6b015c0e301
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-18 23:35:05 -0500

    added list rendering with ngFor directive

--- playground/src/app/list-todos/list-todos.component.ts
+++ playground/src/app/list-todos/list-todos.component.ts
@@ -6,6 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-todos.component.scss']
 })
 export class ListTodosComponent implements OnInit {
+  todos = [{
+    id: 1,
+    description: 'Walk the dog'
+  }, {
+    id: 2,
+    description: 'Clean the dog'
+  }, {
+    id: 3,
+    description: 'Feed the dog'
+  }, {
+    id: 4,
+    description: 'Walk the dog'
+  }]
 
   constructor() { }
 
