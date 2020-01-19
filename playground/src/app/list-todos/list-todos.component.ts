dd890451167bb12e0fe8c8f942e633e601179a7c
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-18 23:42:57 -0500

    converted list items to a class

--- playground/src/app/list-todos/list-todos.component.ts
+++ playground/src/app/list-todos/list-todos.component.ts
@@ -1,24 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 
+
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public done: boolean,
+    public targetDate: Date
+  ) {
+
+  }
+
+
+}
+
+
+
 @Component({
   selector: 'app-list-todos',
   templateUrl: './list-todos.component.html',
   styleUrls: ['./list-todos.component.scss']
 })
 export class ListTodosComponent implements OnInit {
-  todos = [{
-    id: 1,
-    description: 'Walk the dog'
-  }, {
-    id: 2,
-    description: 'Clean the dog'
-  }, {
-    id: 3,
-    description: 'Feed the dog'
-  }, {
-    id: 4,
-    description: 'Walk the dog'
-  }]
+  todos = [new Todo(1, 'Walk the dog'),
+  new Todo(2, 'Clean the dog'),
+  new Todo(3,'Feed the dog'),
+  new Todo(4, 'Walk the dog')
+  ]
 
   constructor() { }
 
