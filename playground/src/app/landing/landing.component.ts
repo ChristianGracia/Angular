e10830bbd2eb2dc8e0f4b7940ac6745603ce8101
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-18 22:30:18 -0500

    added login component

--- playground/src/app/landing/landing.component.ts
+++ playground/src/app/landing/landing.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
+  message = 'hi';
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.message);
   }
 
 }
