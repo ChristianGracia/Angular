aa50237384902bd630fdee6d885e1ee0f4deccf9
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-18 23:04:19 -0500

    added error page component

--- playground/src/app/app.module.ts
+++ playground/src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LandingComponent } from './landing/landing.component';
 import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingComponent,
-    LoginComponent
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
