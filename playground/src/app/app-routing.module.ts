da09b0a20775ad607ad42775ca3ea51c36980969
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-18 23:02:58 -0500

    routes set up

--- playground/src/app/app-routing.module.ts
+++ playground/src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { LandingComponent } from './landing/landing.component';
 
-
-const routes: Routes = [];
+// welcome
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {
+    path: 'landing', component: LandingComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
