bc19d2de35943134950f6c610ab6ded4d3e50fe8
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-18 23:28:28 -0500

    added list todo component

--- playground/src/app/app-routing.module.ts
+++ playground/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { LandingComponent } from './landing/landing.component';
 import { ErrorComponent } from './error/error.component';
+import { ListTodosComponent } from './list-todos/list-todos.component';
 
 // welcome
 const routes: Routes = [
@@ -12,6 +13,7 @@ const routes: Routes = [
   {
     path: 'landing/:name', component: LandingComponent
   },
+  { path: 'todos', component: ListTodosComponent },
   {
     path: '**', component: ErrorComponent
   }
