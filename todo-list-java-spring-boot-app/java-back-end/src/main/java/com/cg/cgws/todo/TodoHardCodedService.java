dd63d17e9d4617720af5a27deb2c0cc7aeb5e9dc
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-21 21:33:36 -0500

    added java backend with get route displaying info on angular front-end

---
+++ todo-list-java-spring-boot-app/java-back-end/src/main/java/com/cg/cgws/todo/TodoHardCodedService.java
@@ -0,0 +1,34 @@
+package com.cg.cgws.todo;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class TodoHardCodedService {
+	private static List<Todo> todos = new ArrayList<Todo>();
+	private static int idCounter = 0;
+	
+	static {
+		todos.add(new Todo(++idCounter, "christian", "Learn to walk", new Date(), false ));
+		todos.add(new Todo(++idCounter, "christian", "Learn to talk", new Date(), false ));
+		todos.add(new Todo(++idCounter, "christian", "Learn to sing", new Date(), false ));
+	}
+	
+	public List<Todo> findAll(){
+		return todos;
+		
+	}
+	public Todo deleteById(long id) {
+		Todo todo = findById(id);
+		todos.remove(todo);
+		
+	}
+	public Todo findById(long id) {
+		
+	}
+
+}
