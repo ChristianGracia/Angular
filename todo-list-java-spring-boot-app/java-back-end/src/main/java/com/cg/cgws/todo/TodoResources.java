5017ad4c8af3def5a067642eaa4d638e42687098
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-22 22:16:22 -0500

    create todo route working

--- todo-list-java-spring-boot-app/java-back-end/src/main/java/com/cg/cgws/todo/TodoResources.java
+++ todo-list-java-spring-boot-app/java-back-end/src/main/java/com/cg/cgws/todo/TodoResources.java
@@ -1,4 +1,5 @@
 package com.cg.cgws.todo;
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +9,11 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @CrossOrigin(origins="http://localhost:4200")
 @RestController
@@ -45,6 +48,14 @@ public class TodoResources {
 	public ResponseEntity<Todo> updateTodo(@PathVariable String username, @PathVariable long id, @RequestBody Todo todo) {
 		Todo todoUpdated = todoService.save(todo);
 		return new ResponseEntity<Todo>(todoUpdated, HttpStatus.OK);
+	}
+	
+	@PostMapping(path="/users/{username}/todos/")
+	public ResponseEntity<Void> createTodo(@PathVariable String username, @RequestBody Todo todo) {
+		Todo newTodo = todoService.save(todo);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newTodo.getId()).toUri();
+		
+		return ResponseEntity.created(uri).build();
 	}
 
 }
