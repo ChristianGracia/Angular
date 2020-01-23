115215d1d83e0ef05f231d516c3debce5b93614c
Christian Gracia <christiangracia.student@careerdevs.com>
2020-01-22 22:05:48 -0500

    update route working on back-end

--- todo-list-java-spring-boot-app/java-back-end/src/main/java/com/cg/cgws/todo/TodoResources.java
+++ todo-list-java-spring-boot-app/java-back-end/src/main/java/com/cg/cgws/todo/TodoResources.java
@@ -2,11 +2,14 @@ package com.cg.cgws.todo;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @CrossOrigin(origins="http://localhost:4200")
@@ -36,6 +39,12 @@ public class TodoResources {
 		}
 		return ResponseEntity.notFound().build();
 	
+	}
+	
+	@PutMapping(path="/users/{username}/todos/{id}")
+	public ResponseEntity<Todo> updateTodo(@PathVariable String username, @PathVariable long id, @RequestBody Todo todo) {
+		Todo todoUpdated = todoService.save(todo);
+		return new ResponseEntity<Todo>(todoUpdated, HttpStatus.OK);
 	}
 
 }
