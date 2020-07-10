14579d7362ce8779645744aaa2c47ca10ce15219
Christian Gracia <christiangracia.student@careerdevs.com>
2020-07-09 23:19:21 -0400

    added routing

--- material/built-out-material-app/src/app/auth/signup/signup.component.spec.ts
+++ material/built-out-material-app/src/app/auth/signup/signup.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { SignnupComponent } from './signnup.component';
+import { SignupComponent } from './signup.component';
 
-describe('SignnupComponent', () => {
-  let component: SignnupComponent;
-  let fixture: ComponentFixture<SignnupComponent>;
+describe('SignupComponent', () => {
+  let component: SignupComponent;
+  let fixture: ComponentFixture<SignupComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ SignnupComponent ]
+      declarations: [ SignupComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(SignnupComponent);
+    fixture = TestBed.createComponent(SignupComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
