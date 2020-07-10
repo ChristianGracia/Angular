4a193f00e8eca04469448a2090a1bc86912267ac
Christian Gracia <christiangracia.student@careerdevs.com>
2020-07-09 22:11:41 -0400

    added a material module

--- material/basic-app-with-material/src/app/app.module.ts
+++ material/basic-app-with-material/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
+
 
 @NgModule({
   declarations: [
@@ -13,6 +15,7 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MaterialModule,
     AppRoutingModule
   ],
   providers: [],
