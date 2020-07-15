addbee51ea89744dde9c6455b058e0937bd71192
Christian Gracia <christiangracia.student@careerdevs.com>
2020-07-14 22:40:45 -0400

    added mat-list to display the menu items in the sidenav on small devices

--- material/built-out-material-app/src/app/material.module.ts
+++ material/built-out-material-app/src/app/material.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 import {
   MatNativeDateModule, MatDatepickerModule,
   MatInputModule, MatFormFieldModule, MatButtonModule,
-  MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule
+  MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule
 } from '@angular/material';
 
 @NgModule({
   imports: [MatButtonModule, MatFormFieldModule, MatInputModule,
-    MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule],
+    MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule],
   exports: [MatButtonModule, MatFormFieldModule, MatInputModule,
-    MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule]
+    MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule]
 
 })
 export class MaterialModule {
