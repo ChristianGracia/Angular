13733b7f46f46284f1bd88488fc21374dc1210d2
Christian Gracia <christiangracia.student@careerdevs.com>
2020-07-14 21:39:25 -0400

    added mat-toolbar + some global styling

--- material/built-out-material-app/src/app/material.module.ts
+++ material/built-out-material-app/src/app/material.module.ts
@@ -3,14 +3,14 @@ import { NgModule } from '@angular/core';
 import {
   MatNativeDateModule, MatDatepickerModule,
   MatInputModule, MatFormFieldModule, MatButtonModule,
-  MatCheckboxModule, MatSidenavModule
+  MatCheckboxModule, MatSidenavModule, MatToolbarModule
 } from '@angular/material';
 
 @NgModule({
   imports: [MatButtonModule, MatFormFieldModule, MatInputModule,
-    MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule],
+    MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule],
   exports: [MatButtonModule, MatFormFieldModule, MatInputModule,
-    MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule]
+    MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule]
 
 })
 export class MaterialModule {
