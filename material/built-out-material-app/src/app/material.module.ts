733a23ed79ca87a731a50d144b8f6a42971dbcfd
Christian Gracia <christiangracia.student@careerdevs.com>
2020-07-15 19:35:05 -0400

    added mat-tab from material for tabs on training screen

--- material/built-out-material-app/src/app/material.module.ts
+++ material/built-out-material-app/src/app/material.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 import {
   MatNativeDateModule, MatDatepickerModule,
   MatInputModule, MatFormFieldModule, MatButtonModule,
-  MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule
+  MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule, MatTabsModule
 } from '@angular/material';
 
 @NgModule({
   imports: [MatButtonModule, MatFormFieldModule, MatInputModule,
-    MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule],
+    MatDatepickerModule, MatNativeDateModule, MatCheckboxModule,
+    MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule, MatTabsModule],
   exports: [MatButtonModule, MatFormFieldModule, MatInputModule,
-    MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule]
+    MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule,
+    MatToolbarModule, MatIconModule, MatListModule, MatTabsModule]
 
 })
 export class MaterialModule {
