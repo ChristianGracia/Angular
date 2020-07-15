0066a5d5a5bf8b397a32cc9f07de8ebbb81a3272
Christian Gracia <christiangracia.student@careerdevs.com>
2020-07-14 21:04:55 -0400

    added mat-checkbox

--- material/built-out-material-app/src/app/material.module.ts
+++ material/built-out-material-app/src/app/material.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 
-import { MatNativeDateModule, MatDatepickerModule, MatInputModule, MatFormFieldModule, MatButtonModule } from '@angular/material';
+import { MatNativeDateModule, MatDatepickerModule, MatInputModule, MatFormFieldModule, MatButtonModule, MatCheckboxModule } from '@angular/material';
 
 @NgModule({
-  imports: [MatButtonModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule],
-  exports: [MatButtonModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule]
+  imports: [MatButtonModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule],
+  exports: [MatButtonModule, MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, MatCheckboxModule]
 
 })
 export class MaterialModule {
