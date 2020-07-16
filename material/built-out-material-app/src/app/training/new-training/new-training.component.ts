35294a64754076d123900def0b2d531593f81a91
Christian Gracia <christiangracia.student@careerdevs.com>
2020-07-15 23:19:38 -0400

    set up a service to let components know what exercise is active with Subjects and Subscriptions

--- material/built-out-material-app/src/app/training/new-training/new-training.component.ts
+++ material/built-out-material-app/src/app/training/new-training/new-training.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { TrainingService } from '../training.service';
 import { Exercise } from '../exercise.model';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-new-training',
@@ -9,17 +10,16 @@ import { Exercise } from '../exercise.model';
 })
 export class NewTrainingComponent implements OnInit {
 
-  @Output() public trainingStart = new EventEmitter<void>();
   exercises: Exercise[] = [];
 
   constructor(private trainingService: TrainingService) { }
 
   ngOnInit() {
-    this.exercises = this.trainingService.availableExercises;
+    this.exercises = this.trainingService.getAvailableExercises();
   }
 
-  onStartTraining() {
-    this.trainingStart.emit();
+  onStartTraining(form: NgForm) {
+    this.trainingService.startExercise(form.value.exercise);
   }
 
 }
--- material/built-out-material-app/src/app/training/training.component.ts
+++ material/built-out-material-app/src/app/training/training.component.ts
@@ -1,16 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { SubscriptionLike } from 'rxjs';
+import { TrainingService } from './training.service';
+
 
 @Component({
   selector: 'app-training',
   templateUrl: './training.component.html',
   styleUrls: ['./training.component.scss']
 })
-export class TrainingComponent implements OnInit {
+export class TrainingComponent implements OnInit, OnDestroy {
+
+  public exerciseSubscription: Subscription;
 
   public ongoingTraining = false;
-  constructor() { }
+  constructor(private trainingService: TrainingService) { }
 
   ngOnInit() {
+    this.exerciseSubscription = this.trainingService.exerciseChanged.subscribe(exercise => {
+      if (exercise) {
+        this.ongoingTraining = true;
+      }
+      else {
+        this.ongoingTraining = false;
+      }
+
+    });
+
+  }
+  ngOnDestroy() {
+    this.exerciseSubscription.unsubscribe();
   }
 
 }
