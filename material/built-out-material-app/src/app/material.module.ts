9bba9aa515729eeeba7c631441adc3823fe6858d
Christian Gracia <christiangracia.student@careerdevs.com>
2020-07-15 20:00:24 -0400

    added mat-select for a dropdown

--- material/built-out-material-app/src/app/material.module.ts
+++ material/built-out-material-app/src/app/material.module.ts
@@ -3,16 +3,18 @@ import { NgModule } from '@angular/core';
 import {
   MatNativeDateModule, MatDatepickerModule,
   MatInputModule, MatFormFieldModule, MatButtonModule,
-  MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule, MatTabsModule, MatCardModule
+  MatCheckboxModule, MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule
 } from '@angular/material';
 
 @NgModule({
   imports: [MatButtonModule, MatFormFieldModule, MatInputModule,
     MatDatepickerModule, MatNativeDateModule, MatCheckboxModule,
-    MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule, MatTabsModule, MatCardModule],
+    MatSidenavModule, MatToolbarModule, MatIconModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule],
+
+
   exports: [MatButtonModule, MatFormFieldModule, MatInputModule,
     MatDatepickerModule, MatNativeDateModule, MatCheckboxModule, MatSidenavModule,
-    MatToolbarModule, MatIconModule, MatListModule, MatTabsModule, MatCardModule]
+    MatToolbarModule, MatIconModule, MatListModule, MatTabsModule, MatCardModule, MatSelectModule]
 
 })
 export class MaterialModule {
