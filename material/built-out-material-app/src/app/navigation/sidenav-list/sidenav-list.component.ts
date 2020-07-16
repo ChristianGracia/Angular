ba02ba7c7bf9479ceef979cd281ff48cc0e27bce
Christian Gracia <christiangracia.student@careerdevs.com>
2020-07-15 21:52:34 -0400

    set up conditional logged in behavior on the side component with rxjs observables + subcriptions

--- material/built-out-material-app/src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ material/built-out-material-app/src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,20 +1,32 @@
-import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output, OnDestroy } from '@angular/core';
+import { Subscription } from 'rxjs/Subscription';
+import { AuthService } from 'src/app/auth/auth.service';
 
 @Component({
   selector: 'app-sidenav-list',
   templateUrl: './sidenav-list.component.html',
   styleUrls: ['./sidenav-list.component.scss']
 })
-export class SidenavListComponent implements OnInit {
+export class SidenavListComponent implements OnInit, OnDestroy {
   @Output() closeSidenavEvent = new EventEmitter<void>();
+  public authSubscription: Subscription;
+  public isAuth: boolean;
 
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.authSubscription = this.authService.authChange.subscribe(authStatus => {
+      this.isAuth = authStatus;
+    })
   }
+
   onClose() {
     this.closeSidenavEvent.emit();
 
+  }
+
+  ngOnDestroy() {
+    this.authSubscription.unsubscribe();
   }
 
 }
